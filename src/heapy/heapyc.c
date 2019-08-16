8f6fd527578e430ba9a2d6a74cc569daa58d430d
YiFei Zhu <zhuyifei1999@gmail.com>
2019-08-16 02:33:26 -0500

    Simplify a lot of C code

--- src/heapy/heapyc.c
+++ src/heapy/heapyc.c
@@ -200,12 +200,8 @@ static struct PyModuleDef moduledef = {
     PyModuleDef_HEAD_INIT,
     MODNAME,
     NULL,
-    0,
-    module_methods,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+    -1,
+    module_methods
 };
 
 static int nyfills() {
