29885239ece94f65069331c76776e41ef0bb0324
YiFei Zhu <zhuyifei1999@gmail.com>
2026-03-31 22:20:07 -0700

    guppy.h: Don't fill tp_new in NYFILL
    Everything that's needed should have a tp_new anyways. No regressions
    found in test suite. If this breaks any use case, we can simply add
    to the type struct:
      .tp_new = PyType_GenericNew,
    
    For #51 finding 24

--- src/include/guppy.h
+++ src/include/guppy.h
@@ -1,9 +1,6 @@
 #ifndef GUPPY_H_INCLUDED
 
 #define NYFILL(t) {                      \
-    if (!t.tp_new) {                     \
-        t.tp_new = PyType_GenericNew;    \
-    }                                    \
     if (PyType_Ready(&t) < 0) return -1; \
 }
 
