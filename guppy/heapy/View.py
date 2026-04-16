45d908daf74401468d6059d649a506eec5daf1e2
YiFei Zhu <zhuyifei1999@gmail.com>
2026-04-15 23:55:10 -0700

    Test another theory

--- guppy/heapy/View.py
+++ guppy/heapy/View.py
@@ -89,9 +89,12 @@ class _GLUECLAMP_:
         return self.mutnodeset()
 
     def clear_check(self):
+        global bad
         ch = self._clear_hook
         try:
+            bad = True
             wr = list(ch)[0]
+            bad = False
         except IndexError:
             self.clear_setup()
         else:
@@ -106,6 +109,7 @@ class _GLUECLAMP_:
                 c[0].cb.callback = None
 
     def clear_callback(self, wr):
+        assert not bad
         self._clear_hook.clear()
         for m in self.clear_methods:
             m()
